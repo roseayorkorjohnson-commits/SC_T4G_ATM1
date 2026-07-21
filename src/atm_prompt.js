00d502655eb2abd24e94293d3de1994a520877ac
ElpeAJ <elpediaarthurjunior@gmail.com>
2026-07-21 14:19:47 +0000

    Adding JS file with steps as comments

---
+++ src/atm_prompt.js
@@ -0,0 +1,27 @@
+//THE VISUAL ATM WORKFLOW
+
+// These two links allow us to print our final results directly onto the webpage screen
+const receiptTitle = document.getElementById("receipt-title");
+const receiptText = document.getElementById("receipt-text");
+
+// 1. SET THEINITIAL BALANCE & SECURITY DATA
+
+
+// 2. ASK THE USER FOR INPUT (Runs automatically as soon as the page loads)
+
+
+// 3. VALIDATE / CHECK THE PIN NUMBER
+
+    
+    
+    
+    
+    // 4. CHECK AVAILABILITY OF FUNDS
+    
+        
+    
+        
+
+    
+    // TASK 6: Handle the error layout if the PIN was completely wrong
+    
