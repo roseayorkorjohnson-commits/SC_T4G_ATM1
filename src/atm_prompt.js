dd08404941427b4da88de95f6d470e39912840b6
roseayorkorjohnson <roseayorkorjohnson@gmail.com>
2026-07-23 05:09:14 -0700

    Added changes

--- src/atm_prompt.js
+++ src/atm_prompt.js
@@ -4,16 +4,35 @@
 const receiptTitle = document.getElementById("receipt-title");
 const receiptText = document.getElementById("receipt-text");
 
-// 1. SET THEINITIAL BALANCE & SECURITY DATA
-
+// 1. SET THE INITIAL BALANCE & SECURITY DATA
+let userPin = "9317";
+let userBalance = 2500;
 
 // 2. ASK THE USER FOR INPUT (Runs automatically as soon as the page loads)
-
+let enteredPin = prompt("Welcome to Nova Bank ATM. Please enter your 4-digit pin:");
 
 // 3. VALIDATE / CHECK THE PIN NUMBER
+ if(enteredPin === userPin){
+   let userAmount = prompt(`PIN ACCEPTED! Your current balance is ${userBalance}. Enter amount to withdraw `);
+   console.log(typeof (userAmount)); //string
+   userAmount = Number (userAmount); //reassigning the userAmount variable
+   //console.log(typeof (userAmount)) // number
+   if (userBalance >= userAmount){
+      let newuserBalance = userBalance - userAmount;
 
-    
-    
+      receiptText.innerText ="TRANSACTION SUCCESSFUL";
+      receiptText.innerText =`Dispensing cash... You withdrew ${userAmount}.
+      Your remaining balance is now ${userBalance - userAmount}`;
+   } else{
+      receiptText.innerText = "INSUFFICIENT FUNDS"
+      receiptText.innerText = `Transaction declined ... You cannot withdraw ${userAmount}, because your account balance ${userBalance}`
+   }
+ }     else{
+          receiptTitle.innerText = "ACCESS DENIED";
+          receiptText.innerText = "Invalid security PIN code entered. Account blocked for your protection";
+          }       
+     
+       
     
     
     // 4. CHECK AVAILABILITY OF FUNDS
